1a85f4d0df816c00fded3b4c72ceac8939679d01
manishvbn <manishvbn@gmail.com>
2022-06-11 22:59:29 +0530

    Day2 Start

---
+++ Day2/react-core-app/src/components/root/RootComponent.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+import React from 'react';
+
+const RootComponent = () => {
+    return (
+        <div className='container'>
+        </div>
+    );
+};
+
+export default RootComponent;
