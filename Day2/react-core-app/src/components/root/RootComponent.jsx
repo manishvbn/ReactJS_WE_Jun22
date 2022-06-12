a35e915926eb8320710fbe2f961c0bb9c5c3badc
manishvbn <manishvbn@gmail.com>
2022-06-12 12:54:14 +0530

    ControlledVsUncontrolledComponent

--- Day2/react-core-app/src/components/root/RootComponent.jsx
+++ Day2/react-core-app/src/components/root/RootComponent.jsx
@@ -5,6 +5,7 @@ import LCDemoComponent from '../1_lifecycle-demo/LCDemoComponent';
 import PureImpureComponent from '../2_pure-vs-impure/PureImpureComponent';
 import EventComponent from '../3_synthetic-events/EventComponent';
 import CounterAssignment from '../4_counter-assignment/CounterAssignment';
+import ControlledVsUncontrolledComponent from '../5_controlled-vs-uncontrolled/ControlledVsUncontrolledComponent';
 
 const RootComponent = () => {
     return (
@@ -12,7 +13,8 @@ const RootComponent = () => {
             {/* <LCDemoComponent /> */}
             {/* <PureImpureComponent /> */}
             {/* <EventComponent /> */}
-            <CounterAssignment />
+            {/* <CounterAssignment /> */}
+            <ControlledVsUncontrolledComponent />
         </div>
     );
 };
