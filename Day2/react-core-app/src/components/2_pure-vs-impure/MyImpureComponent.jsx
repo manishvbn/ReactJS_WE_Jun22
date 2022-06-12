8f6ed8fff7db158590d17130e38601db6934c33c
manishvbn <manishvbn@gmail.com>
2022-06-12 10:18:37 +0530

    PureImpure Updated

--- Day2/react-core-app/src/components/2_pure-vs-impure/MyImpureComponent.jsx
+++ Day2/react-core-app/src/components/2_pure-vs-impure/MyImpureComponent.jsx
@@ -1,25 +1,64 @@
+// import React, { Component } from 'react';
+
+// class MyImpureComponent extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { count: 0 };
+//     }
+
+//     render() {
+//         console.log("MyImpureComponent - Render Called....");
+//         return (
+//             <h1>
+//                 Count Value is: {this.state.count}
+//             </h1>
+//         );
+//     }
+
+//     componentDidMount() {
+//         setInterval(() => {
+//             // this.setState({ count: this.state.count + 1 });
+//             this.setState({ count: 0 });
+//         }, 1000);   
+//     }
+// }
+
+// export default MyImpureComponent;
+
+// ------------------------------------------------------------------------ With Reference
+
 import React, { Component } from 'react';
 
 class MyImpureComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = { count: 0 };
+        this.state = { numbers: [1, 2] };
     }
 
     render() {
         console.log("MyImpureComponent - Render Called....");
         return (
             <h1>
-                Count Value is: {this.state.count}
+                Array Length is: {this.state.numbers.length}
             </h1>
         );
     }
 
     componentDidMount() {
+        // setInterval(() => {
+        //     var last = this.state.numbers[this.state.numbers.length - 1];
+        //     this.state.numbers.push((last + 1));
+        //     this.setState({ numbers: this.state.numbers }, () => {
+        //         console.log(this.state.numbers);
+        //     });
+        // }, 1000);
+
         setInterval(() => {
-            // this.setState({ count: this.state.count + 1 });
-            this.setState({ count: 0 });
-        }, 1000);   
+            var last = this.state.numbers[this.state.numbers.length - 1];
+            this.setState({ numbers: [...this.state.numbers, (last + 1)] }, () => {
+                console.log(this.state.numbers);
+            });
+        }, 1000);
     }
 }
 
--- Day2/react-core-app/src/components/2_pure-vs-impure/MyPureComponent.jsx
+++ Day2/react-core-app/src/components/2_pure-vs-impure/MyPureComponent.jsx
@@ -34,28 +34,64 @@
 
 // ----------------------------------------------------------------
 
+// import React, { PureComponent } from 'react';
+
+// class MyPureComponent extends PureComponent {
+//     constructor(props) {
+//         super(props);
+//         this.state = { count: 0 };
+//     }
+
+//     render() {
+//         console.log("MyPureComponent - Render Called....");
+//         return (
+//             <h1>
+//                 Count Value is: {this.state.count}
+//             </h1>
+//         );
+//     }
+
+//     componentDidMount() {
+//         setInterval(() => {
+//             this.setState({ count: 0 });
+//         }, 1000);
+//     }
+// }
+
+// export default MyPureComponent;
+
+// ----------------------------------------------------------------
+
 import React, { PureComponent } from 'react';
 
 class MyPureComponent extends PureComponent {
     constructor(props) {
         super(props);
-        this.state = { count: 0 };
+        this.state = { numbers: [1, 2] };
     }
 
     render() {
         console.log("MyPureComponent - Render Called....");
         return (
             <h1>
-                Count Value is: {this.state.count}
+                Array Length is: {this.state.numbers.length}
             </h1>
         );
     }
 
     componentDidMount() {
         setInterval(() => {
-            this.setState({ count: 0 });
+            var last = this.state.numbers[this.state.numbers.length - 1];
+            this.setState({ numbers: [...this.state.numbers, (last + 1)] }, () => {
+                console.log(this.state.numbers);
+            });
         }, 1000);
     }
+
+    // We can Immutable.js to work with Immutability
 }
 
 export default MyPureComponent;
+
+// React.PureComponent's shouldComponentUpdate() only shallowly compares the objects.
+// React.PureComponent's shouldComponentUpdate() skips prop updates for the whole component subtree.
