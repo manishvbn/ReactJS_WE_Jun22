8d875a50d7e50beeb7d7078736326499fb4d4a81
manishvbn <manishvbn@gmail.com>
2022-06-18 09:50:14 +0530

    CRUD Assignment Solution

--- Day3/react-core-app/src/components/root/RootComponent.jsx
+++ Day3/react-core-app/src/components/root/RootComponent.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable */
 
 import React from 'react';
+import CrudAssignment from '../1_crud/CrudAssignment';
 import ErrorHandler from '../common/ErrorHandler';
 
 const RootComponent = () => {
     return (
         <div className='container'>
             <ErrorHandler>
+                <CrudAssignment />
             </ErrorHandler>
         </div>
     );
