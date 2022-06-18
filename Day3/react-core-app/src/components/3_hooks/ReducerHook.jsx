a05616ef00be3aba5714a3aaf49386f51f0431c5
manishvbn <manishvbn@gmail.com>
2022-06-18 12:50:13 +0530

    ReducerHook

---
+++ Day3/react-core-app/src/components/3_hooks/ReducerHook.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+// Pure Function
+// const counterReducer = (state, action) => {
+//     switch (action.type) {
+//         case 'increment':
+//             return { count: state.count + 1 };
+//         case 'decrement':
+//             return { count: state.count - 1 };
+//         default:
+//             throw new Error('Invalid Action executed');
+//     }
+// }
+
+// const Counter = (props) => {
+//     const [state, dispatch] = useReducer(counterReducer, initialState);
+
+//     return (
+//         <React.Fragment>
+//             <div className="text-center mb-5">
+//                 <h3 className="text-primary">Counter Component</h3>
+//             </div>
+//             <div className="d-grid gap-2 mx-auto col-6">
+//                 <h2 className="text-info text-center">
+//                     {state.count}
+//                 </h2>
+//                 <button className="btn btn-info"
+//                     onClick={() => {
+//                         dispatch({ type: 'increment' })
+//                     }}>
+//                     <span className='fs-4'>+</span>
+//                 </button>
+//                 <button className="btn btn-info"
+//                     onClick={() => {
+//                         dispatch({ type: 'decrement' })
+//                     }}>
+//                     <span className='fs-4'>-</span>
+//                 </button>
+//             </div>
+//         </React.Fragment >
+//     );
+// }
+
+const counterReducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + action.payload };
+        case 'decrement':
+            return { count: state.count - action.payload };
+        default:
+            throw new Error('Invalid Action executed');
+    }
+}
+
+const Counter = (props) => {
+    const [state, dispatch] = useReducer(counterReducer, initialState);
+
+    return (
+        <React.Fragment>
+            <div className="text-center mb-5">
+                <h3 className="text-primary">Counter Component</h3>
+            </div>
+            <div className="d-grid gap-2 mx-auto col-6">
+                <h2 className="text-info text-center">
+                    {state.count}
+                </h2>
+                <button className="btn btn-info"
+                    onClick={() => {
+                        dispatch({ type: 'increment', payload: 5 })
+                    }}>
+                    <span className='fs-4'>+</span>
+                </button>
+                <button className="btn btn-info"
+                    onClick={() => {
+                        dispatch({ type: 'decrement', payload: 5  })
+                    }}>
+                    <span className='fs-4'>-</span>
+                </button>
+            </div>
+        </React.Fragment >
+    );
+}
+
+// Will Have a different Copy of State
+const CounterSibling = () => {
+    const [state, dispatch] = useReducer(counterReducer, initialState);
+
+    return (
+        <div className="text-center">
+            <h3 className="text-success">Counter Sibling Component</h3>
+            <h2>Current Count is: {state.count}</h2>
+        </div>
+    );
+}
+
+const ReducerHook = () => {
+    return (
+        <div>
+            <Counter />
+            <hr />
+            <CounterSibling />
+        </div>
+    );
+};
+
+export default ReducerHook;
--- Day3/react-core-app/src/components/root/RootComponent.jsx
+++ Day3/react-core-app/src/components/root/RootComponent.jsx
@@ -5,6 +5,7 @@ import CrudAssignment from '../1_crud/CrudAssignment';
 import AjaxComponent from '../2_ajax/AjaxComponent';
 import EffectHookAjax from '../3_hooks/EffectHookAjax';
 import EffectHookDemo from '../3_hooks/EffectHookDemo';
+import ReducerHook from '../3_hooks/ReducerHook';
 import SiblingCommunication from '../3_hooks/SiblingCommunication';
 import StateHookDemo from '../3_hooks/StateHookDemo';
 import ErrorHandler from '../common/ErrorHandler';
@@ -18,7 +19,8 @@ const RootComponent = () => {
                 {/* <StateHookDemo /> */}
                 {/* <EffectHookDemo /> */}
                 {/* <EffectHookAjax /> */}
-                <SiblingCommunication />
+                {/* <SiblingCommunication /> */}
+                <ReducerHook />
             </ErrorHandler>
         </div>
     );
