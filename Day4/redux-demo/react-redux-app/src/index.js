7dc5d3f9eeb3d49b6adfcc90cf64c5351c1e0555
manishvbn <manishvbn@gmail.com>
2022-06-19 12:41:02 +0530

    First 4 Steps Implemented - Redux

--- Day4/redux-demo/react-redux-app/src/index.js
+++ Day4/redux-demo/react-redux-app/src/index.js
@@ -8,11 +8,41 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap';
 import RootComponent from './components/root/RootComponent';
 
+import configureStore from './store/configureStore';
+import * as counterActions from './actions/counterActions';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <RootComponent />
+  <RootComponent />
   // </React.StrictMode>
 );
 
 reportWebVitals();
+
+// const appStore = configureStore();
+// console.log(appStore);
+// console.log(appStore.getState());
+
+// const appStore = configureStore({ counterReducer: 1000 });
+// console.log(appStore);
+// console.log(appStore.getState());
+
+const appStore = configureStore();
+console.log("Loaded State:", appStore.getState());
+
+appStore.subscribe(() => {
+  console.warn("Store State Change Detected");
+  console.log("New State:", appStore.getState());
+});
+
+// Take the actions to be performed from Action Creator
+// Dispatch actions to modify the store
+
+let incAction = counterActions.incCounter();
+let decAction = counterActions.decCounter();
+
+// When we dispatch the action, reducer will take it and modify the store
+appStore.dispatch(incAction);
+appStore.dispatch(incAction);
+appStore.dispatch(decAction);
---
+++ Day4/redux-demo/react-redux-app/src/store/configureStore.js
@@ -0,0 +1,10 @@
+// 1. Basic Configuration
+import { createStore } from "redux";
+import rootReducer from "../reducers/rootReducer";
+
+export default function configureStore(preloadedState) {
+    return createStore(
+        rootReducer,
+        preloadedState
+    );
+}
